c1aead92e46213858510b478961725f47e6b0aa2
AmaanSiddiqui1804 <amaansiddiqui1804@gmail.com>
2025-03-06 14:05:51 +0530

    Amaan Siddiqui: Add. UC_02: Logo

--- angular-intro/src/app/app.component.spec.ts
+++ angular-intro/src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hello-world-app' title`, () => {
+  it(`should have as title 'angular-intro'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world-app');
+    expect(app.title).toEqual('angular-intro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello-world-app');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-intro app is running!');
   });
 });
